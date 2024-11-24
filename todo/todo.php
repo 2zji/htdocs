fceef3da2337ae84555b50a3abf0b2dbc0023c4e
Yeji Yun <142556812+2zji@users.noreply.github.com>
2024-11-24 23:02:49 +0900

    수정

--- todo/todo.php
+++ todo/todo.php
@@ -40,7 +40,7 @@ mysqli_close($conn);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>할 일 관리</title>
-    <link rel="stylesheet" href="styles.css"> <!-- styles.css 연결 -->
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <div class="container">
