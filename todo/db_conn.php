12f61534c3c6bb23a81e76dad1681b5260bf5ac2
Yeji Yun <142556812+2zji@users.noreply.github.com>
2024-12-13 00:25:03 +0900

    필요없는 db연결 줄 삭제

--- todo/db_conn.php
+++ todo/db_conn.php
@@ -5,8 +5,3 @@ $password = "111111"; // 사용자의 MySQL 비밀번호
 $dbname = "testdb";   // 사용할 데이터베이스 이름
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-?>
