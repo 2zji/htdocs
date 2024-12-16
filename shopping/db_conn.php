3fcfd3e933ad4186e617c75b94c912e87d83c593
Yeji Yun <142556812+2zji@users.noreply.github.com>
2024-12-16 23:25:49 +0900

    db_conn 생성

---
+++ shopping/db_conn.php
@@ -0,0 +1,8 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "111111";
+    $dbname = "testdb";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
