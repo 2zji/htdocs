544e7bd5c46fa3234049850622d2307956b000a8
Yeji Yun <142556812+2zji@users.noreply.github.com>
2025-03-21 19:38:53 +0900

    test.1

---
+++ PHPtester/script.js
@@ -0,0 +1,5 @@
+const lightBulb = document.querySelector('.light-bulb');
+const switchButton = document.getElementById('switch');
+switchButton.addEventListener('click', () => {
+lightBulb.classList.toggle('on');
+});
